efc27a40d650509949b3420a1d69dcd7a87ded09
Chris Ortman <chrisortman@gmail.com>
2011-04-16 00:05:40 -0500

    First commit

---
+++ chrisortman.com/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace chrisortman.com.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+        	ViewBag.Title = "Welcome to my homepage!";
+            return View();
+        }
+
+    }
+}
---
+++ chrisortman.com/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="chrisortman.com.MvcApplication" Language="C#" %>
